9332159c7632c94bf1e8a45bcc2f3efe9c1e13e8
xpulido <xpulido37@gmail.com>
2024-07-31 11:50:19 -0500

    cam

--- CarritoApp/views.py
+++ CarritoApp/views.py
@@ -1,55 +1,68 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from CarritoApp.models import LineaPedido, Pedido
-from .carrito import Carrito
-from ElBuenSaborApp.models import Producto,Adicion
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMultiAlternatives
 from django.utils.html import strip_tags
 from django.template.loader import render_to_string
+from django.conf import settings
+from CarritoApp.models import LineaPedido, Pedido
+from ElBuenSaborApp.models import Producto, Adicion
+from .carrito import Carrito
 
 def qr_view(request):
+    if request.method == 'POST':
+        imagen = request.FILES.get('imagen')
+
+        # Aquí debes obtener los datos necesarios para la factura
+         # Debes obtener el pedido asociado al comprobante de pago
+          # Debes obtener las líneas de pedido asociadas al pedido
+        nombre = request.POST.get('nombre')
+        direccion = request.POST.get('direccion')
+        telefono = request.POST.get('telefono')
+        texto = request.POST.get('texto')
+
+        pedido = None
+        lineas_pedidos = []
+
+        enviar_correo_con_comprobante(imagen, pedido, lineas_pedidos, nombre, direccion, telefono, texto)
+
+        return redirect('pedido')  # Redirige a donde sea necesario después de enviar el correo
+
     return render(request, 'qr.html')
 
-def Menu(request):
-    lista = Producto.objects.all()
-    return render(request, 'menu.html', {'productos': lista})
 
-def pedido(request):
-    productos = Producto.objects.all()
-    adicion=Adicion.objects.all()
-    return render(request, 'carrito.html', {'productos': productos,'adiciones':adicion})
+def enviar_correo_con_comprobante(imagen, pedido, lineas_pedidos, nombre, direccion, telefono, texto):
+    # Preparar el correo electrónico
+    subject = 'Comprobante de pago adjunto y factura del pedido'
+    from_email = settings.EMAIL_HOST_USER
+    to_email = ['web.kmx3@gmail.com']  # Dirección de correo a donde enviar
 
-def comprar(request):
-    productos = Producto.objects.all()
-    adicion=Adicion.objects.all()
-    return render(request, 'info.html', {'productos': productos,'adiciones':adicion})
+    # Renderizar el contenido HTML de la factura
+    html_content = render_to_string('pedido.html', {
+        'pedido': pedido,
+        'lineas_pedidos': lineas_pedidos,
+        'nombre': nombre,
+        'direccion': direccion,
+        'telefono': telefono,
+        'texto': texto,
+    })
 
-def agregar_producto(request, producto_id):
-    carrito = Carrito(request)
-    producto = Producto.objects.get(id=producto_id)
-    carrito.agregar(producto)
-    messages.success(request, (f'El producto {producto} se ha agregado al carrito.'))
-    return redirect('menu')
+    # Extraer el texto sin formato del contenido HTML
+    text_content = strip_tags(html_content)
 
-def eliminar_producto(request, producto_id):
-    carrito = Carrito(request)
-    producto = Producto.objects.get(id=producto_id)
-    carrito.eliminar(producto)
-    messages.warning(request, f'El producto {producto} se ha eliminado del carrito.')
-    return redirect('pedido')
+    # Crear el objeto de mensaje de correo electrónico
+    msg = EmailMultiAlternatives(subject, text_content, from_email, to_email)
+    msg.attach_alternative(html_content, "text/html")
 
-def restar_producto(request, producto_id):
-    carrito = Carrito(request)
-    producto = Producto.objects.get(id=producto_id)
-    carrito.restar(producto)
-    messages.warning(request, (f'Se ha restado 1 unidad del producto {producto}.'))
-    return redirect('pedido')
+    # Adjuntar el comprobante de pago al correo
+    msg.attach(imagen.name, imagen.read(), imagen.content_type)
 
-def limpiar_carrito(request):
-    carrito = Carrito(request)
-    carrito.limpiar()
-    messages.info(request, 'El carrito se ha vaciado.')
-    return redirect('menu')
+    # Enviar el correo electrónico
+    try:
+        msg.send()
+    except Exception as e:
+        # Manejar errores si ocurre algún problema en el envío del correo
+        print(f'Error al enviar correo electrónico: {e}')
+        raise  # O maneja el error según tus requerimientos
 
 
 def procesar_pedido(request):
@@ -104,6 +117,56 @@ def procesar_pedido(request):
 
 
 
+
+def Menu(request):
+    lista_productos = Producto.objects.all()
+    lista_adiciones = Adicion.objects.all()
+    return render(request, 'menu.html', {'productos': lista_productos, 'adiciones': lista_adiciones})
+
+
+def pedido(request):
+    lista_productos = Producto.objects.all()
+    lista_adiciones = Adicion.objects.all()
+    return render(request, 'carrito.html', {'productos': lista_productos, 'adiciones': lista_adiciones})
+
+
+def comprar(request):
+    lista_productos = Producto.objects.all()
+    lista_adiciones = Adicion.objects.all()
+    return render(request, 'info.html', {'productos': lista_productos, 'adiciones': lista_adiciones})
+
+
+def agregar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.agregar(producto)
+    messages.success(request, f'El producto {producto} se ha agregado al carrito.')
+    return redirect('menu')
+
+
+def eliminar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.eliminar(producto)
+    messages.warning(request, f'El producto {producto} se ha eliminado del carrito.')
+    return redirect('pedido')
+
+
+def restar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.restar(producto)
+    messages.warning(request, f'Se ha restado 1 unidad del producto {producto}.')
+    return redirect('pedido')
+
+
+def limpiar_carrito(request):
+    carrito = Carrito(request)
+    carrito.limpiar()
+    messages.info(request, 'El carrito se ha vaciado.')
+    return redirect('menu')
+
+
 def enviar_email(**kwargs):
     pedido = kwargs.get("pedido")
     lineas_pedidos = kwargs.get("lineas_pedidos")
@@ -133,40 +196,4 @@ def enviar_email(**kwargs):
     to = kwargs.get("email_usuario")
 
     send_mail(asunto, mensaje_texto, from_email, [to], html_message=mensaje)
-    
-# from django.shortcuts import render
-# from django.core.mail import EmailMessage
-# from django.conf import settings
 
-# def procesar_formulario(request):
-#     if request.method == 'POST':
-        
-#         imagen = request.FILES.get('imagen', None)
-
-#         if imagen:
-#             Procesar la imagen si es necesario
-#             Aquí podrías guardarla en el sistema de archivos o hacer otras operaciones
-
-#             Preparar el correo electrónico
-#             subject = 'Comprobante de pago'
-#             message = 'Se adjunta el comprobante de pago.'
-#             from_email = settings.EMAIL_HOST_USER
-#             to_email = ['destinatario@example.com']  # Reemplaza con el destinatario deseado
-
-#             Crear el objeto de mensaje de correo electrónico
-#             email = EmailMessage(subject, message, from_email, to_email)
-#             email.attach(imagen.name, imagen.read(), imagen.content_type)  # Adjuntar la imagen al correo
-
-#             try:
-#                 email.send()  # Envía el correo electrónico
-#             except Exception as e:
-#                 Maneja cualquier error en el envío del correo electrónico
-#                 Aquí podrías registrar el error en un log o mostrar un mensaje al usuario
-#                 print(f'Error al enviar correo electrónico: {e}')
-#                 return render(request, 'error.html')  # Renderiza una página de error
-
-#             Si el correo se envió correctamente, puedes redirigir o mostrar un mensaje de éxito
-#             return render(request, 'exito.html')  # Renderiza una página de éxito o redirige a donde sea necesario
-
-#     Si no es un método POST o no se envió correctamente, renderiza el formulario nuevamente
-#     return render(request, 'qr.html')  # Renderiza el formulario de carga de imagen nuevamente
